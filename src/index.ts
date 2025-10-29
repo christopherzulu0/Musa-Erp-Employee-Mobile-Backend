362dc71e759168e4219094474c7f492fdef375df
unknown <christopherzulu04@gmail.com>
2025-10-29 13:18:27 +0200

    db url

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: '.env' });
 
 // Set DATABASE_URL explicitly if not loaded
 if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = 'postgresql://postgres:87064465@localhost:5432/hr?schema=public';
+  process.env.DATABASE_URL = 'postgresql://neondb_owner:npg_cbHgZ5PSkaD3@ep-blue-paper-adkqzq45-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require';
 }
 
 import cors from 'cors';
